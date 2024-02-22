b259564153ac59bc0fc1cb3175a51aa39de01f94
MarcusRowcliffe <marcus.rowcliffe@ioz.ac.uk>
2024-02-22 19:48:43 +0000

    Example file update

--- camtrapDensity_example.R
+++ camtrapDensity_example.R
@@ -1,41 +1,44 @@
 # See https://github.com/MarcusRowcliffe/camtrapDensity for detailed instructions
 
-# INITIAL SETUP
+# 1. INITIAL SETUP
 # One-off
 install.packages(c("devtools", "dply", "tidyr", "jsonlite", "lubridate"))
 devtools::install_github("inbo/camtraptor")
 devtools::install_github("MarcusRowcliffe/camtrapDensity")
 
-# LOAD PACKAGES
+# 2. LOAD PACKAGES
 # At the beginning of each session
 library(camtrapDensity)
 library(camtraptor)
 library(lubridate)
 
-# LOAD DATA
+# 3. LOAD DATA
 # Assumes data are in the root project directory and Windows OS.
 # Delete the "." for Mac.
 pkg <- read_camtrapDP("./datapackage.json")
 
-# CHECK DEPLOYMENT SCHEDULE
+# 4. SUBSET DEPLOYMENTS (OPTIONAL)
+# Selects only deployments occuring within 2023
+# Edit date as necessary
+subpkg <- subset_deployments(pkg, start > ymd("2023-01-01") &
+                               end < ymd("2024-01-01"))
+
+# 5. CHECK DEPLOYMENT SCHEDULE
 plot_deployment_schedule(pkg)
 
-# SUBSET DEPLOYMENTS
-# Example selects only deployments starting in 2023 or later and rechecks schedule
-# Edit date as necessary
-subpkg <- subset_deployments(pkg, start > ymd("2023-01-01"))
-plot_deployment_schedule(subpkg)
+# 6. CHECK DEPLOYMENT CALIBRATION MODELS
+pkg_chk <- check_deployment_models(subpkg)
 
-# CHECK DEPLOYMENT CALIBRATION MODELS
-pkg_chk <- check_deployment_models(pkg)
-
-# REM ANALYSIS
+# 7. REM ANALYSIS
 res <- rem_estimate(pkg_chk, check_deployments=FALSE)
 
-# EVALUATE MODEL FITS
+# 8. EVALUATE MODEL FITS
 plot(res$activity_model)
 plot(res$radius_model, pdf=TRUE)
 plot(res$angle_model)
 
-# EXTRACT MODEL ESTIMATES
+# 9. EXTRACT MODEL ESTIMATES
 res$estimates
+
+# 10. SAVE WORKSPACE FOR FUTURE REFERENCE
+save.image()
