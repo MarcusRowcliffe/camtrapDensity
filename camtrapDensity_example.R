084157403203cc9bff92483d8075de944a08997b
MarcusRowcliffe <marcus.rowcliffe@ioz.ac.uk>
2024-06-20 23:57:24 +0100

    Example update

--- camtrapDensity_example.R
+++ camtrapDensity_example.R
@@ -15,16 +15,25 @@ library(lubridate)
 # 3. LOAD DATA
 # Assumes data are in the root project directory and Windows OS.
 # Delete the "." for Mac.
-pkg <- read_camtrapDP("./datapackage.json")
+pkg <- read_camtrapDP("./DataPackage_V1.0/datapackage.json")
+plot_deployment_schedule(pkg)
 
-# 4. SUBSET DEPLOYMENTS (OPTIONAL)
-# Selects only deployments occuring within 2023
-# Edit date as necessary
-subpkg <- subset_deployments(pkg, start > ymd("2023-01-01") &
-                               end < ymd("2024-01-01"))
+# 4. FILTER DATA (IF NECESSARY)
+# e.g. selects only deployments occurring within 2023
+subpkg <- subset_deployments(pkg, choice=end < ymd("2024-10-20"))
+# slices all deployments to given date
+subpkg <- slice_camtrap_dp(pkg,
+                           start = "2017/10/09",
+                           end = "2017/10/26")
+# slices deployment at location S02 to end at the given date
+subpkg <- slice_camtrap_dp(pkg,
+                           end = "2017/10/27",
+                           depChoice = locationName=="S02")
+plot_deployment_schedule(subpkg)
 
 # 5. CHECK DEPLOYMENT SCHEDULE
 plot_deployment_schedule(pkg)
+plot_deployment_schedule(subpkg)
 
 # 6. CHECK DEPLOYMENT CALIBRATION MODELS
 pkg_chk <- check_deployment_models(subpkg)
